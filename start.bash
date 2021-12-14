211ba9b4c863c14dc10055df14a4a19a5d119857
valpr <me@andrewfong.ca>
2021-12-14 01:28:15 -0500

    convenience and fix docs

--- start.bash
+++ start.bash
@@ -2,24 +2,24 @@
 
 # This script will run the current AOC based on day of the month
 # options:
-# $1: day to run
-# $2: part to run (1 or 2)
-# $3: input to use (u for input.txt, p for practiceInput.txt)
-# ex: "npm start 1 07 p" runs day 7 part 1 on practice input
+# $1: part to run (1 | 2)
+# $2: input to use (u for input.txt, p for practiceInput.txt)
+# $3: day to run
+# ex: "npm start 1 p 07" runs part 1 practice input for day 7
 # default behaviour is run both parts for current day of the month
-day=$(date +"%d") && [[ "$2" != "" ]] && day=$2
+day=$(date +"%d") && [[ "$3" != "" ]] && day=$3
 userInput="./input/day$day/input.txt"
 practiceInput="./input/day$day/practiceInput.txt"
 
 if [[ $1 != "" ]]
 then
     echo "Day $day part $1:"
-    if [[ ($3 = "p") || $3 = "" ]]
+    if [[ ($2 = "p") || $2 = "" ]]
     then
     echo "Practice input:"
     npx ts-node "src/day$day/part0$1.ts" $practiceInput
     fi
-    if [[ ($3 = "u") || $3 = "" ]]
+    if [[ ($2 = "u") || $2 = "" ]]
     then
     echo "Real input:"
     npx ts-node "src/day$day/part0$1.ts" $userInput
