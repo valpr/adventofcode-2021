20351a877a3cd0505a174532dc48c457c1a36316
valpr <me@andrewfong.ca>
2021-12-24 17:16:09 -0500

    finally makeup for day 19aoc

---
+++ src/day19/part01.ts
@@ -0,0 +1,111 @@
+import { readFileSync } from 'fs';
+const fileName = process.argv[2]
+
+const distance = (coord1: number[], coord2: number[]) => {
+    return coord1.reduce((acc, current, idx) => {
+        return acc + Math.abs(current- coord2[idx])
+    }, 0)
+}
+
+// create a map of each beacon with three nearest neightbours
+const createNeighbourMap = (beacons: number[][]) => {
+    let neighbourMap: {[key: number]: number[][]} = {}
+    for (const beacon of beacons){
+        let peerDistances : {[key: number]: number[]} = {}
+        for (const peerBeacon of beacons){
+            if (peerBeacon != beacon){
+                peerDistances[distance(beacon, peerBeacon)] = peerBeacon
+            }
+        }
+        // find the two nearest beacon neighbours
+        let sortedPDistances = Object.keys(peerDistances).map(el => parseInt(el)).sort((a,b) => a-b)
+        let [closestA, closestB] = [sortedPDistances[0], sortedPDistances[1]]
+        // create a hash of the distance between all 3 neighbours
+        let [neighbourA, neighbourB] = [peerDistances[closestA], peerDistances[closestB]]
+        let hash = (closestA + closestB) * distance(neighbourA, neighbourB)
+        neighbourMap[hash] = [beacon, neighbourA, neighbourB]
+    }
+    return neighbourMap
+}
+
+const findMatchingNeighbours = (fieldMap: {[key: number]: number[][]}, scannerMaps: any[] ) => {
+    for (const fieldHash of Object.keys(fieldMap)){
+        for (const [scanner, scannerMap] of scannerMaps){
+            for (const scannerHash of Object.keys(scannerMap)){
+                if (scannerHash === fieldHash){
+                    let fieldNeighbours = fieldMap[parseInt(scannerHash)]
+                    let scannerNeighbours = scannerMap[scannerHash]
+                    return [scanner, fieldNeighbours, scannerNeighbours]
+                }
+            }
+        }
+    }
+}
+
+const findOrientation = (fieldNeighbours: any, scannerNeighbours: any): [any, any, number[]] => {
+    let offset = Array(3).fill(null)
+    let direction = Array(3).fill(null)
+    let rotation = Array(3).fill(null)
+    for (let i = 0 ; i < 3; i ++){
+        if (offset[i] != null){
+            continue;
+        }
+        for (let j = 0; j < 3 ; j++){
+            for (let k of [-1, 1]){
+                let offsets = new Set()
+                for (let l = 0; l < 3; l++){
+                        offsets.add(fieldNeighbours[l][i] - scannerNeighbours[l][j] * k)
+                }
+                if (offsets.size === 1){
+                    offset[i] = Array.from(offsets.values())[0]
+                    direction[i] = k
+                    rotation[i] = j
+                }
+            }
+        }
+    }
+    return [offset, direction, rotation]
+}
+
+const reorientCoords = (orientation: [any, any, number[]], beacons: any[]) => {
+    let [offset, direction, rotation] = orientation
+    return beacons.map((beacon: number[]) => {
+        return beacon.map((_coord, index: number) => beacon[rotation[index]] * direction[index] + offset[index])
+    })
+}
+
+let scannerList: number[][][] = readFileSync(fileName, 'utf-8').split('\n\n').map(el => el.split('\n').slice(1).map(coords => coords.split(',').map(coord => parseInt(coord))));
+
+let firstScanner = scannerList.shift() || scannerList[0]
+let field: {[key: string]: number[]} = {}
+for (const values of firstScanner){
+    field[`${values}`] = values
+}
+let scannerLocations = []
+let scannerMaps = scannerList.map((scanner) => [scanner, createNeighbourMap(scanner)])
+
+while (scannerMaps.length > 0){
+    let fieldMap = createNeighbourMap(Object.values(field))
+    let [scanner, fieldNeighbours, scannerNeighbours]: any = findMatchingNeighbours(fieldMap, scannerMaps)
+
+    for (let i = 0; i < scannerMaps.length; i++){
+        if (scannerMaps[i][0] === scanner){
+            scannerMaps.splice(i, 1)
+            break;
+        }
+    }
+
+    let orientation = findOrientation(fieldNeighbours, scannerNeighbours)
+    let [scannerCoord, ...vars] = orientation
+    
+    scannerLocations.push(scannerCoord)
+
+    let newCoords = reorientCoords(orientation, scanner)
+    for (const coord of newCoords){
+        field[`${coord}`] = coord
+    }
+}
+
+let beaconCount = Object.keys(field).length
+
+console.log('beacons: ', beaconCount)
